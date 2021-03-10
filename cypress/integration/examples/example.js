5c265be2a8c6aa46a990a9f03c7ae9e2a1e9b261
melibe23 <32499737+melibe23@users.noreply.github.com>
2021-03-10 14:23:17 +0100

    Update to use [key]

--- cypress/integration/examples/example.js
+++ cypress/integration/examples/example.js
@@ -1,36 +1,43 @@
-// The last attempt works. 
+import {
+  paths,
+  keys
+} from "../../support/data";
 
 describe(`RegExp issue`, () => {
-  beforeEach(() => {
-    cy.fixture("example.json").as("fixture");
-    cy.visit(`https://www.getapp.com/website-ecommerce-software/ecommerce/`)
-  });
+  paths.forEach((path, index) => {
+    const key = keys[index];
+    beforeEach(() => {
+      cy.fixture("example.json").as("fixture");
+      cy.visit(`https://www.getapp.com/website-ecommerce-software/ecommerce/`)
 
-  it("First attempt - Fails", function () {
-    cy.get('head meta[name="description"]').then(p => {
-      expect(p).to.have.attr("content", this.fixture.description);
+    });
+
+    it("First attempt - Fails", function () {
+      cy.get('head meta[name="description"]').then(p => {
+        expect(p).to.have.attr("content", this.fixture.description);
+      });
+    });
+
+    it("Second attempt - Fails", function () {
+      cy.get('head meta[name="description"]').then(p => {
+        const regExp = new RegExp(this.fixture.description)
+        expect(p).to.have.attr("content", regExp);
+      });
+    });
+
+    it.only("Third attempt - Fails", function () {
+      cy.get('head meta[name="description"]').then(p => {
+        const regExp = new RegExp(this.fixture[key].description)
+        expect(p).to.have.attr("content").match(regExp);
+      });
+    });
+
+    it("Fourth attempt - PASSED", function () {
+      cy.get('head meta[name="description"]').then(p => {
+        expect(p).to.have.attr("content").match(/^Reviewing [0-9]+ of the best ecommerce software applications.  GetApp lets you compare the list of tools and vendors that provide ecommerce software solutions.$/);
+      });
     });
   });
-
-  it("Second attempt - Fails", function () {
-    cy.get('head meta[name="description"]').then(p => {
-      const regExp = new RegExp(this.fixture.description)
-      expect(p).to.have.attr("content", regExp);
-    });
-  });
-
-  it("Third attempt - Fails", function () {
-    cy.get('head meta[name="description"]').then(p => {
-      const regExp = new RegExp(this.fixture.description)
-      expect(p).to.have.attr("content").match(regExp);
-    });
-  });
-
-  it("Fourth attempt - PASSED", function () {
-    cy.get('head meta[name="description"]').then(p => {
-      expect(p).to.have.attr("content").match(/^Reviewing [0-9]+ of the best ecommerce software applications.  GetApp lets you compare the list of tools and vendors that provide ecommerce software solutions.$/);
-    });
-  });
-});
+})
 
 // Because the type of testing I have to do, I cannot go with the 4th attemp. 
---
+++ cypress/support/data.js
@@ -0,0 +1,6 @@
+  export const pathsForSEOTags = {
+      category: "next/customer-management-software/crm/"
+  };
+
+  export const paths = Object.values(pathsForSEOTags);
+  export const keys = Object.keys(pathsForSEOTags);
