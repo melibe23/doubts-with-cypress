e78af308c3c583d9443ee64ea91ed85e8e8b8c85
melibe23 <32499737+melibe23@users.noreply.github.com>
2021-08-10 13:13:46 +0200

    Added expected

--- cypress/integration/visual-mobile.js
+++ cypress/integration/visual-mobile.js
@@ -3,7 +3,7 @@ import { viewportsObj, userAgents, viewportsNames, browser } from '../support/de
 const path = `https://example.cypress.io/`
 
 describe(`${browser}-hero-image`, () => {
-  viewportsObj.forEach((viewport, i) => {
+  viewportsObj.slice(0,1).forEach((viewport, i) => {
     context(`${viewportsNames[0]}`, viewport, () => {
       before(() => {
         cy.visit(path, {
@@ -15,7 +15,8 @@ describe(`${browser}-hero-image`, () => {
       })
 
       it('Log-user-agent', () => {
-        console.log(`UserAgent: ${navigator.userAgent}`)
+        console.log(`Expected UserAgent: ${userAgents[0]}`)
+        console.log(`navigator.userAgent: ${navigator.userAgent}`)
         // logs: UserAgent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36
       })
 
